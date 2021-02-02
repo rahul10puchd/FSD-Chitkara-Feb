b37d15e2b000754067876a2752aeda418b91e572
Rahul Kumar <rahul10.pu@gmail.com>
2021-02-02 14:42:58 +0530

    Added comments for HelloWorldServelt program

--- HelloWorldUsingServelt/src/main/java/upgrad/HelloWorldApp.java
+++ HelloWorldUsingServelt/src/main/java/upgrad/HelloWorldApp.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@SpringBootApplication
-@ServletComponentScan
+@SpringBootApplication  //this defines that you are using SpringBoot here to create a web application.
+//Spring Boot comes with an embedded Tomcat server, which provides you with the servlet container.
+@ServletComponentScan   //this annotation tells your application class to look for the servlet class you have created annotated with @WebServlet.
 public class HelloWorldApp {
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApp.class, args);
--- HelloWorldUsingServelt/src/main/java/upgrad/HelloWorldServlet.java
+++ HelloWorldUsingServelt/src/main/java/upgrad/HelloWorldServlet.java
@@ -7,8 +7,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-@WebServlet("/gethello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet("/gethello")//denotes that this class is a Java web servlet
+public class HelloWorldServlet extends HttpServlet //responds to HTTP requests
+{   // servlet container invokes the service method of the appropriate servlet class based on the request.
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         PrintWriter p=response.getWriter();// opens up the stream
         p.print("<!Doctype html>");
@@ -17,6 +18,6 @@ public class HelloWorldServlet extends HttpServlet {
         p.print("<p>Helllllllloo</p>");
         p.print("</body>");
         p.print("</html>");
-        p.close();
+        p.close();// close the stream
     }
 }
