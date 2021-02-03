dea2893bd0df3a9f4fba695317c7378d1c1dfe6b
Rahul Kumar <rahul10.pu@gmail.com>
2021-02-03 10:04:55 +0530

    updated comment

--- HelloWorldUsingServelt/src/main/java/upgrad/HelloWorldServlet.java
+++ HelloWorldUsingServelt/src/main/java/upgrad/HelloWorldServlet.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 @WebServlet("/gethello")//denotes that this class is a Java web servlet
 public class HelloWorldServlet extends HttpServlet //responds to HTTP requests
-{   // servlet container invokes the service method of the appropriate servlet class based on the request.
+{
+    // servlet container invokes the service method of the appropriate servlet class based on the request.
     public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         PrintWriter p=response.getWriter();// opens up the stream
         p.print("<!Doctype html>");
